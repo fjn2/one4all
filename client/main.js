ebc196c4a550b9ba965cb1224eaf40265f3facab
Federico Naveira <federiconaveira@gmail.com>
2017-06-19 00:01:15 -0300

    Auto download song improved

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ class ServerTime {
   constructor(intercommunication) {
     this.intercommunication = intercommunication;
     this.detour = undefined; // desvio
-    this.maxSampleritems = 5;
+    this.maxSampleritems = 30;
     this.sampler = [];
   }
   getSampler() {
@@ -146,60 +146,28 @@ class AudioPlayer {
       this.audioElement.controls = true;
     }
     this.src = './beep.mp3';
-    window.foo = this.audioElement;
+
     this.serverTime = serverTime;
 
     window.document.body.appendChild(this.audioElement);
-    // status = 0 -> no initialized 1 -> requesting current track 2 -> downloading file  4 -> track loaded
-    this.status = 0;
-    this.cachedSongs = {};
   }
-  loadAudio(playList) {
-    this.status = 1;
-    this.setSong(undefined);
+  loadAudio() {
     this.intercommunication.get('currentTrack', ({ data }) => {
       if (data) {
-        if (this.cachedSongs[data]) {
-          this.setSong(this.cachedSongs[data]);
-          this.status = 4;
+        if (downloader.cachedSongs[data] && downloader.cachedSongs[data].tmpUrl) {
+          if (this.audioElement.src !== downloader.cachedSongs[data].tmpUrl) {
+            this.setSong(downloader.cachedSongs[data].tmpUrl);
+          }
         } else {
-          this.downloadSong(data).then(() => {
-            this.loadAudio(playList);
+          console.log('The song is not ready');
+          downloader.startDownload(data, true).then(() => {
+            console.log('Trying loading again!');
+            this.loadAudio();
           });
-        }
-        const nextTrack = playList[playList.indexOf(data) + 1] || playList[0];
-        if (nextTrack) {
-          this.downloadSong(nextTrack);
         }
       } else {
         console.error('The play list looks like empty dude :(');
       }
-    });
-  }
-  downloadSong(filename) {
-    return new Promise((resolve) => {
-      this.status = 2;
-      const xhttp = new XMLHttpRequest();
-      xhttp.addEventListener('progress', (e) => {
-        if (e.lengthComputable) {
-          const percentComplete = Math.round((e.loaded / e.total) * 100);
-          this.percentEl.innerHTML = `${filename} - ${percentComplete}`;
-          console.log(`Downloading: (${filename}) ${percentComplete}%`);
-        }
-      });
-
-      xhttp.addEventListener('load', () => {
-        if (xhttp.status == 200) {
-          const tmpUrl = window.URL.createObjectURL(xhttp.response);
-          this.cachedSongs[filename] = tmpUrl;
-          this.status = 4;
-          resolve();
-        }
-      });
-
-      xhttp.open('GET', filename);
-      xhttp.responseType = 'blob';
-      xhttp.send();
     });
   }
   setSong(songURL = '') {
@@ -217,8 +185,8 @@ class AudioPlayer {
 
       if (timeDifference >= 0 && !Number.isNaN(this.serverTime.getDetour())) {
         setTimeout(() => {
+          document.getElementById('playDiff').innerHTML = Math.round((trackTime + delay - 100) - this.audioElement.currentTime * 1000) + 'ms';
           this.seek(trackTime + delay);
-          document.getElementById('playDiff').innerHTML = Math.round((trackTime + delay) - this.audioElement.currentTime * 1000) + 'ms';
           const initialTime = new Date();
           // 100ms looping to have a better performance
           while (new Date() - initialTime < 100) {}
@@ -271,10 +239,9 @@ class PlayList {
       this.songs = songs;
 
       if (this.currentSong !== currentSong) {
-        this.audioPlayer.loadAudio(this.songs);
         this.audioPlayer.stop();
+        this.audioPlayer.loadAudio();
       }
-
       this.currentSong = currentSong;
 
       this.render();
@@ -293,9 +260,17 @@ class PlayList {
 
     for (let i = 0; i < this.songs.length; i += 1) {
       const song = this.songs[i];
+      let percent = 'Download this song now!';
+      let currentSongClass = this.currentSong === song ? 'current-song' : '';
+      if (downloader.cachedSongs[song]) {
+        percent = downloader.cachedSongs[song].percentComplete;
+      }
+      if (percent === 100) {
+        percent = 'Ready to play! :)';
+      }
       el += `
       <ul>
-        <li>${song}</li>
+        <li class="${currentSongClass}">${song} - <b onclick="downloadSong('${song}')">${percent}</b></li>
       </ul>
       `;
     }
@@ -324,19 +299,77 @@ class Chat {
     window.document.getElementById('activityStream').innerHTML += `${message} <br/>`;
   }
 }
+class Downloader {
+  constructor(playList) {
+    this.playList = playList.song;
+    this.cachedSongs = {};
+  }
+  // patrol:bool -> will loop over all songs to check for new downloads
+  startDownload(filename, patrol = false) {
+    let promise;
+    if (!this.cachedSongs[filename]) {
+      this.cachedSongs[filename] = {};
+      promise = new Promise((resolve) => {
+        const xhttp = new XMLHttpRequest();
+        xhttp.addEventListener('progress', (e) => {
+          if (e.lengthComputable) {
+            const percentComplete = Math.round((e.loaded / e.total) * 100);
+            this.cachedSongs[filename].percentComplete = percentComplete;
+            console.log(`Downloading: (${filename}) ${percentComplete}%`);
+            playList.render();
+          }
+        });
+
+        xhttp.addEventListener('load', () => {
+          if (xhttp.status == 200) {
+            const tmpUrl = window.URL.createObjectURL(xhttp.response);
+            this.cachedSongs[filename].tmpUrl = tmpUrl;
+            resolve();
+          }
+        });
+
+        xhttp.open('GET', filename);
+        xhttp.responseType = 'blob';
+        xhttp.send();
+      });
+    } else {
+      if (this.cachedSongs[filename].tmpUrl) {
+        console.log('This song is already downloaded', filename);
+      } else {
+        console.log('Download is already in progress', filename);
+      }
+      promise = new Promise((resolve) => {
+        // wait for 5 second to check the next song
+        setTimeout(resolve, 5000);
+      });
+    }
+    if (patrol) {
+      promise.then(() => {
+        // Once this finish, start downloading the next song
+        let nextSong = playList.songs[playList.songs.indexOf(filename) + 1];
+        if (!nextSong) {
+          nextSong = playList.songs[0];
+        }
+        this.startDownload(nextSong, true);
+      });
+    }
+    return promise;
+  }
+}
+
 class App {
   constructor() {
     const percentEl = window.document.getElementById('percent');
 
     this.intercommunication = new Intercommunication(configuration.server);
     this.serverTime = new ServerTime(this.intercommunication);
+    this.downloader = new Downloader(this.intercommunication);
     this.audioPlayer = new AudioPlayer(this.intercommunication, this.serverTime, percentEl);
     this.playList = new PlayList('playList', this.intercommunication, this.audioPlayer);
     this.chat = new Chat(this.intercommunication);
 
     this.serverTime.startSynchronization();
 
-    // this.audioPlayer.loadAudio();
     this.playList.waitForPlayList();
     this.playList.waitForNumberOfConections();
     this.chat.waitForActivityStream();
@@ -372,6 +405,13 @@ class App {
 // application starts
 
 const app = new App();
+// expose the object to the entry world
+const intercommunication = app.intercommunication;
+const serverTime = app.serverTime;
+const downloader = app.downloader;
+const audioPlayer = app.audioPlayer;
+const playList = app.playList;
+const chat = app.chat;
 
 
 function addSongToPlayList() {
@@ -407,4 +447,7 @@ function isMobile() {
   var check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
+}
+function downloadSong(song) {
+  downloader.startDownload(song);
 }
